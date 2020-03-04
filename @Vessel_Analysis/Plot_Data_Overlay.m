a3235ef289b07b50552f3b1aa4d738f531580c3a
Johannes Rebling <johannesrebling@gmail.com>
2020-03-04 14:40:19 +0100

    data overlay plotting

--- @Vessel_Analysis/Plot_Data_Overlay.m
+++ @Vessel_Analysis/Plot_Data_Overlay.m
@@ -57,32 +57,74 @@ function Plot_Data_Overlay(AVA)
   end
 
   
+  fprintf('Drawing them lines...');
   outIm = nan(size(AVA.xy));
   imSize = size(outIm);
   nVessel = numel(vList);
-  figure(); 
-  iH = imagesc(outIm);
-  axis image;
-  colormap('jet');
   for iVessel = 1:nVessel
-    fprintf('vessel %i/%i\n',iVessel,nVessel);
     vessel = vList(iVessel);
     side1 = vessel.side1(vessel.keep_inds,:);
     side2 = vessel.side2(vessel.keep_inds,:);
     angles = vessel.angles(vessel.keep_inds,:);
     angles = atan2d(angles(:, 2), angles(:, 1));
+    angles(angles < 0) = angles(angles < 0) + 180; % only use 0 - 180 deg
     nSegments = size(side1,1);
     for iSeg = 1:nSegments
       % [x, y] = bresenham( side1(iSeg, 1), side1(iSeg, 2), ...
       %                     side2(iSeg, 1), side2(iSeg, 2));
       [x, y] = xiaolinwu(side1(iSeg, 1), side1(iSeg, 2), ...
-                          side2(iSeg, 1), side2(iSeg, 2));
+      side2(iSeg, 1), side2(iSeg, 2));
       linIdx = sub2ind(imSize, x, y);
       outIm(linIdx) = angles(iSeg);
     end
   end
-  % outIm = inpaint_nans(outIm);
-  set(iH, 'cData', outIm);
-  drawnow();
+  fprintf('done!\n');
+
+  fprintf('Filling them missing values...');
+  % try and fill in missing values but do it fast...
+  outIm1 = fillmissing(outIm, 'linear', 2, 'EndValues', 'nearest');
+  outIm2 = fillmissing(outIm, 'linear', 1, 'EndValues', 'nearest');
+    % outIm = imgaussfilt(outIm, depthMapSmooth);
+  outIm = (outIm2 + outIm1)./2; 
+  fprintf('done!\n');
+  
+  % create background RGB image ------------------------------------------------
+  num_colors = 256;
+  maskBackCMap = gray(256);
+  maskFrontCMap = hsv(256);
+  back = normalize(AVA.xy); % already normalized at this point
+  % scale the background image from 0 to num_colors
+  % back = imadjust(back);
+  back = round(num_colors .* back);
+  % convert the background image to true color
+  back = ind2rgb(back, maskBackCMap);
+
+  outIm = outIm./180; % normalize to 0-1
+  frontIm = round(num_colors .* outIm);
+  % convert the depth mask to true color
+  frontIm = ind2rgb(frontIm, maskFrontCMap);
+  
+  depthImage = back .* frontIm;
+  
+  figure();
+  subplot(2,3,1)
+  imagesc(back); axis image;
+  
+  subplot(2,3,4)
+  imagesc(frontIm); axis image;
+  
+  subplot(2,3,[2 6])
+  imagesc(depthImage);
+  colormap(maskFrontCMap); axis image;
+
+  % figure(); 
+  % iH = imagesc(outIm);
+  % axis image;
+  % colormap('hsv');
+  % % outIm = inpaint_nans(outIm);
+  % set(iH, 'cData', outIm);
+  % drawnow();
+
+
 
 end
